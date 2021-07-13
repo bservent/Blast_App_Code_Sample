344f175faf6a30ee039c85790ee2b5c5172f6fb3
Brittany Servent <bservent@gmail.com>
2021-07-13 11:26:47 -0700

    css, django, and templates added

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>BLAST OFF!</h1>')
+    return render(request, 'home.html')
+
+def result(request):
+    return render(request, 'result.html')
